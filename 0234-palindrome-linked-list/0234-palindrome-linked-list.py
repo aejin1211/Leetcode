15721ea54343d18e77bc679f0a091fc64d8f811e
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-08 13:07:27 -0700

    Time: 30 ms (65.43%), Space: 39.6 MB (14.57%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q: List = []
+        q: Deque = collections.deque()
 
         if not head:
             return True
@@ -16,7 +16,7 @@ class Solution:
             node = node.next
 
         while len(q) > 1:
-            if q.pop(0) != q.pop():
+            if q.popleft() != q.pop():
                 return False
-        return True
         
+        return True
