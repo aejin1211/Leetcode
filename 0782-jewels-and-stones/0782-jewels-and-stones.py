1a6f53b9ebcb05b7b695cad3e8c36ae550ab42bf
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-19 14:12:34 -0700

    Time: 3 ms (9.54%), Space: 17.6 MB (82.48%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,13 +1,7 @@
 class Solution:
     def numJewelsInStones(self, cc: str, stones: str) -> int:
-        freqs = collections.defaultdict(int)
+        freqs = collections.Counter(stones)
         count = 0
-
-        for char in stones:
-            freqs[char] += 1
-
         for char in cc:
             count += freqs[char]
-            
         return count
-
