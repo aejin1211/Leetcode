46a1a465c02ce8bf3e42ac9951e3e929e2bf186e
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-19 14:13:21 -0700

    Time: 1 ms (15.06%), Space: 17.7 MB (51.97%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -2,6 +2,8 @@ class Solution:
     def numJewelsInStones(self, cc: str, stones: str) -> int:
         freqs = collections.Counter(stones)
         count = 0
+
         for char in cc:
             count += freqs[char]
+
         return count
