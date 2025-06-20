c02cde767ed0db87ac80250d024f27e692d3aee4
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-19 19:43:18 -0700

    Time: 3 ms (90.93%), Space: 21.2 MB (80.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,8 +7,8 @@ class Solution:
             heapq.heappush(freqs_heap, (-freqs[f], f))
 
         topk = list()
+
         for _ in range(k):
             topk.append(heapq.heappop(freqs_heap)[1])
-
         return topk
         
