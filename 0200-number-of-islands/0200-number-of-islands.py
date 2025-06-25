bce8c4ea9ea1e2fcf33856a36cad82b921ceaa7f
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-24 19:17:20 -0700

    Time: 254 ms (47.9%), Space: 20.1 MB (68.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,12 +5,14 @@ class Solution:
                 j < 0 or j >= len(grid[0]) or \
                 grid[i][j] != '1':
                 return
+
             grid[i][j] = 0
 
             dfs(i+1, j)
             dfs(i-1, j)
             dfs(i, j+1)
             dfs(i, j-1)
+
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
