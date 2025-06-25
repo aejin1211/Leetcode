81125041a778ce0b74ca77fc9875b44661f0daea
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-24 23:58:37 -0700

    Time: 127 ms (83.41%), Space: 59.7 MB (28.06%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,6 +1,8 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         results = []
+
+
         def dfs(elements, start: int, k: int):
             if k == 0:
                 results.append(elements[:])
@@ -10,7 +12,7 @@ class Solution:
                 elements.append(i)
                 dfs(elements, i+1, k-1)
                 elements.pop()
-
         dfs([], 1, k)
         return results
-        
+
+
