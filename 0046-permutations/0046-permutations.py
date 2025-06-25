53257732529a4bf86a2146fd019bee1eb7ab0b70
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-06-24 23:07:40 -0700

    Time: 4 ms (11.63%), Space: 17.9 MB (81.26%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,11 +2,10 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         results = []
         prev_elements = []
-
         def dfs(elements):
             if len(elements) == 0:
                 results.append(prev_elements[:])
-
+            
             for e in elements:
                 next_elements = elements[:]
                 next_elements.remove(e)
@@ -14,5 +13,10 @@ class Solution:
                 prev_elements.append(e)
                 dfs(next_elements)
                 prev_elements.pop()
+
         dfs(nums)
         return results
+
+
+
+        
