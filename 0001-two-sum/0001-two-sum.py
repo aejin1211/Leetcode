e2bf7f9525f7ed1af7f40df3f1c984fd32624859
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-09-13 00:48:16 -0700

    Time: 3 ms (50.21%), Space: 18.9 MB (50.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,8 +3,7 @@ class Solution:
         nums_map = {}
 
         for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [i, nums_map[target - num]]
             nums_map[num] = i
 
-        for i, num in enumerate(nums):
-            if target - num in nums_map and i != nums_map[target-num]:
-                return [i, nums_map[target - num]]
