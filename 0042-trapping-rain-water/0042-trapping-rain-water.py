50f3d724093e939a6be75fc2001a119edaf04691
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-09-18 01:17:55 -0700

    Time: 14 ms (53.68%), Space: 19.3 MB (69.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,17 +3,17 @@ class Solution:
         if not height:
             return 0
 
-        volume = 0
         left, right = 0, len(height) - 1
         left_max, right_max = height[left], height[right]
 
+        volume = 0
         while left < right:
-            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
-
+            left_max, right_max = max(left_max, height[left]), max(right_max, height[right])
             if left_max <= right_max:
                 volume += left_max - height[left]
                 left += 1
             else:
                 volume += right_max - height[right]
                 right -= 1
-        return volume
+        
+        return volume 
