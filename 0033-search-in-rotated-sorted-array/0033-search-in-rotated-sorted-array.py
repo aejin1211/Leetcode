220df169ee31c9e7aacb5115e0347bc276e231b8
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-11-04 16:31:58 -0800

    Time: 0 ms (100%), Space: 18.2 MB (22.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,15 +3,15 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l + r) // 2
+            mid = (l+r) // 2
             if target == nums[mid]:
                 return mid
-            
             if nums[l] <= nums[mid]:
                 if target > nums[mid] or target < nums[l]:
                     l = mid + 1
                 else:
                     r = mid - 1
+
             else:
                 if target < nums[mid] or target > nums[r]:
                     r = mid - 1
