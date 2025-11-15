5148f7647d4d5982240383b2e982e5103f9c99a1
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2025-11-15 15:39:45 -0800

    Time: 0 ms (100%), Space: 54.5 MB (94.18%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -5,7 +5,7 @@
  */
 var twoSum = function(numbers, target) {
     let l = 0;
-    let r = numbers.length -1;
+    let r = numbers.length - 1;
 
     while (l < r){
         const curSum = numbers[l] + numbers[r];
@@ -15,9 +15,8 @@ var twoSum = function(numbers, target) {
         } else if (curSum < target){
             l++;
         } else{
-            return [l+1, r+1];
+            return [l + 1, r + 1];
         }
-
+        
     }
-    return []
 };
