831c749aa2dcee60b0617095b8b135bca21cf194
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-01-21 00:04:27 -0800

    Time: 3 ms (51.18%), Space: 20.6 MB (5.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = {}
-    
+
         for i, num in enumerate(nums):
             if dict and target - num in dict:
                 return [i, dict[target-num]]
-
-            dict[num] = i
             
+            dict[num] = i
