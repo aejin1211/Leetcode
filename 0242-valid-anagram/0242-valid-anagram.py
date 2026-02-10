0a216ec9d89296048839585c01fcb43992ae1af5
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-02-10 02:39:38 -0800

    Time: 7 ms (90.7%), Space: 19.4 MB (45.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,7 +5,7 @@ class Solution:
 
         countS = collections.defaultdict(int)
         countT = collections.defaultdict(int)
-
+        
         for i in range(len(s)):
             countS[s[i]] += 1
             countT[t[i]] += 1
@@ -14,3 +14,4 @@ class Solution:
             if countS[char] != countT[char]:
                 return False
         return True
+            
