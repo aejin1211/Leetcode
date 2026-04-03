8b8be95d223ee1aad5a77ae233b01c8db30c470d
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-04-03 14:53:10 -0700

    Time: 0 ms (100%), Space: 19.4 MB (64.04%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.py
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -2,8 +2,11 @@ class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
         n = len(nums)
 
-        def next_index(curr):
-            return (curr + nums[curr]) % n
+        def next_index(i):
+            return (i + nums[i]) % n
+
+        def valid(i, direction):
+            return nums[i] != 0 and (nums[i] > 0) == direction
 
         for i in range(n):
             if nums[i] == 0:
@@ -14,40 +17,30 @@ class Solution:
             fast = i
 
             while True:
-                # slow 한 칸 이동 가능 여부 확인
-                next_slow = next_index(slow)
-                if nums[slow] == 0 or (nums[slow] > 0) != direction:
+                slow_next = next_index(slow)
+                fast_next = next_index(fast)
+
+                if not valid(slow_next, direction):
                     break
-                if nums[next_slow] == 0 or (nums[next_slow] > 0) != direction:
+                if not valid(fast_next, direction):
                     break
 
-                # fast 한 칸 이동 가능 여부 확인
-                next_fast = next_index(fast)
-                if nums[fast] == 0 or (nums[fast] > 0) != direction:
-                    break
-                if nums[next_fast] == 0 or (nums[next_fast] > 0) != direction:
+                fast_next2 = next_index(fast_next)
+                if not valid(fast_next2, direction):
                     break
 
-                # 실제 이동
-                slow = next_slow
-                fast = next_index(next_fast)
-
-                # fast 두 번째 이동 후도 방향 확인
-                if nums[fast] == 0 or (nums[fast] > 0) != direction:
-                    break
+                slow = slow_next
+                fast = fast_next2
 
                 if slow == fast:
-                    # self-loop 방지
                     if slow == next_index(slow):
                         break
                     return True
 
-            # 이번 시작점에서 지나간 경로는 0으로 마킹
             curr = i
-            while nums[curr] != 0 and (nums[curr] > 0) == direction:
+            while valid(curr, direction):
                 nxt = next_index(curr)
                 nums[curr] = 0
                 curr = nxt
 
         return False
-       
