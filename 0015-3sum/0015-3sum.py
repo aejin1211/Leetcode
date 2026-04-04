5e845421e0d7a6e983e6621c0a7cbc1e796e3d18
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-04-04 12:48:54 -0700

    Time: 587 ms (74.18%), Space: 22.3 MB (64.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,29 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res = []
         nums.sort()
 
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
-
-            if nums[i] == nums[i-1] and i > 0:
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i-1]:
                 continue
-            while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                if sum < 0:
-                    left += 1
-                elif sum > 0:
-                    right -= 1
-                else:
-                    result.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-        return result           
 
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                sum = num + nums[l] + nums[r]
+                if sum > 0:
+                    r -= 1
+                elif sum < 0:
+                    l += 1
+                else:
+                    res.append([num, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
                     
+
+        return res
+
+
+        
