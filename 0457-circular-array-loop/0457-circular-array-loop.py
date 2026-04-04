6fe807f6e8d1b071f8e68a0c0d4678b106323a48
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-04-04 13:26:25 -0700

    Time: 303 ms (19.21%), Space: 19.3 MB (86.75%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.py
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -2,45 +2,40 @@ class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
         n = len(nums)
 
-        def next_index(i):
-            return (i + nums[i]) % n
+        def next_index(curr):
+            return (curr + nums[curr]) % n
 
-        def valid(i, direction):
-            return nums[i] != 0 and (nums[i] > 0) == direction
+        def valid(num, direction):
+            return (num > 0) == direction
+                
 
         for i in range(n):
-            if nums[i] == 0:
-                continue
-
-            direction = nums[i] > 0
             slow = i
             fast = i
-
+            direction = nums[i] > 0
+            #Positive: True, Negatvie: False
+        
             while True:
                 slow_next = next_index(slow)
                 fast_next = next_index(fast)
 
-                if not valid(slow_next, direction):
+                if not valid(nums[slow_next], direction):
                     break
-                if not valid(fast_next, direction):
+                if not valid(nums[fast_next], direction):
                     break
-
-                fast_next2 = next_index(fast_next)
-                if not valid(fast_next2, direction):
+                
+                fast_next = next_index(fast_next)
+                if not valid(nums[fast_next], direction):
                     break
-
+                
                 slow = slow_next
-                fast = fast_next2
+                fast = fast_next
 
                 if slow == fast:
                     if slow == next_index(slow):
                         break
                     return True
 
-            curr = i
-            while valid(curr, direction):
-                nxt = next_index(curr)
-                nums[curr] = 0
-                curr = nxt
-
         return False
+
+        
