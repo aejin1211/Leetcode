052f895c16c7b29ae4f2c375530281171d7fa957
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-04-06 09:08:39 -0700

    Time: 309 ms (17.49%), Space: 19.3 MB (63.12%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.py
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -1,41 +1,38 @@
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
+        # k > 1 
+        # numbers all positive, all negative
         n = len(nums)
 
-        def next_index(curr):
-            return (curr + nums[curr]) % n
+        def next_index(index):
+            return (index + nums[index]) % n
 
         def valid(num, direction):
             return (num > 0) == direction
-                
 
         for i in range(n):
             slow = i
             fast = i
-            direction = nums[i] > 0
-            #Positive: True, Negatvie: False
-        
-            while True:
-                slow_next = next_index(slow)
-                fast_next = next_index(fast)
 
-                if not valid(nums[slow_next], direction):
+            direction = nums[i] > 0
+            # True: positive, False: negative
+
+            while True:
+                slow = next_index(slow)
+                fast = next_index(fast)
+
+                if not valid(nums[slow], direction):
                     break
-                if not valid(nums[fast_next], direction):
+
+                if not valid(nums[fast], direction):
                     break
                 
-                fast_next = next_index(fast_next)
-                if not valid(nums[fast_next], direction):
+                fast = next_index(fast)
+                if not valid(nums[fast], direction):
                     break
-                
-                slow = slow_next
-                fast = fast_next
 
                 if slow == fast:
                     if slow == next_index(slow):
                         break
                     return True
-
         return False
-
-        
