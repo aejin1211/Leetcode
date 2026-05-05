788bb1019c9725039afa2599d26bbc036f34fd41
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-05-05 00:20:01 -0700

    Time: 315 ms (14.31%), Space: 19.4 MB (54.05%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.py
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -1,38 +1,47 @@
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
+        # same direction 
         # k > 1 
-        # numbers all positive, all negative
         n = len(nums)
 
-        def next_index(index):
-            return (index + nums[index]) % n
+        def next_index(i):
+            return (i + nums[i]) % n
 
-        def valid(num, direction):
-            return (num > 0) == direction
+        def valid(direction, i):
+            return (nums[i] > 0) == direction
+
 
         for i in range(n):
+
+            direction = nums[i] > 0
             slow = i
             fast = i
 
-            direction = nums[i] > 0
-            # True: positive, False: negative
-
             while True:
-                slow = next_index(slow)
-                fast = next_index(fast)
+                slow_next = next_index(slow)
+                fast_next = next_index(fast)
 
-                if not valid(nums[slow], direction):
+                if not valid(direction, slow_next):
                     break
-
-                if not valid(nums[fast], direction):
+                if not valid(direction, fast_next):
                     break
                 
-                fast = next_index(fast)
-                if not valid(nums[fast], direction):
+                fast_next2 = next_index(fast_next)
+                if not valid(direction, fast_next2):
                     break
-
+                
+                slow = slow_next
+                fast = fast_next2
+            
                 if slow == fast:
                     if slow == next_index(slow):
                         break
                     return True
-        return False
+        return False    
+            # curr = i
+            # while valid(direction, curr):
+            #     nxt = next_index(curr)
+            #     nums[curr] = 0
+            #     curr = nxt
+            
+
