71de7e46cdcdf971382e91eb998da90ea6cdd808
Aejin Lee <57307486+aejin1211@users.noreply.github.com>
2026-07-12 10:17:44 -0500

    Time: 13 ms (32.35%), Space: 22.8 MB (69.03%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,19 +7,21 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-        
+            
         while len(lists) > 1:
-            mergedLists = []
+            mergedList = []
             for i in range(0, len(lists), 2):
-                l1 =lists[i]
+                l1 = lists[i]
                 l2 = lists[i+1] if (i+1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists =mergedLists
+                mergedList.append(self.mergeList(l1, l2))
+            lists = mergedList
+        
         return lists[0]
-    
+
     def mergeList(self, l1, l2):
         dummy = ListNode()
         tail = dummy
+
         while l1 and l2:
             if l1.val < l2.val:
                 tail.next = l1
@@ -27,12 +29,11 @@ class Solution:
             else:
                 tail.next = l2
                 l2 = l2.next
-            
             tail = tail.next
-            if l1:
-                tail.next =l1
+
         if l1:
             tail.next = l1
         if l2:
-            tail.next= l2
+            tail.next = l2
         return dummy.next
+            
